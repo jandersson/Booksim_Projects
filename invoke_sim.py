e21649fc13aa856d229b50a145d5368771ce4169
Jonas Andersson <jonas@andersson.io>
2014-11-29 12:40:23 -0600

    Update config file with new injection rates

--- invoke_sim.py
+++ invoke_sim.py
@@ -1,9 +1,13 @@
-__author__ = 'jonas'
+__author__ = 'Jonas Andersson'
 
 from subprocess import call
+import re
+import fileinput
 
 #Global State
 config_filename = "sim_config"
+inject_rate = re.compile(r"(?P<identifier>injection_rate)(?P<assign_op>[ =]+)(?P<value>[.0-9]+)(?P<semicolon>;)")
+rates = [round(rate * 0.01, 3) for rate in range(100)]
 
 
 #Helper Functions
@@ -14,6 +18,11 @@ def invoke():
     """
     call(["booksim", config_filename])
 
+
 #Main Loop
 if __name__ == '__main__':
-    invoke()
+    for rate in rates:
+        with fileinput.input(config_filename, inplace=True) as config:
+            for line in config:
+                print(inject_rate.sub('injection_rate = ' + str(rate) + ';', line), end='')
+        invoke()
