e48dbb50a7ebaed66c8e63a990d58c9a8e339477
Jonas Andersson <jonas@andersson.io>
2014-11-29 14:05:52 -0600

    invoke function writes only the overall traffic statistics to file

--- invoke_sim.py
+++ invoke_sim.py
@@ -7,6 +7,7 @@ import fileinput
 #Global State
 config_filename = "sim_config"
 inject_rate = re.compile(r"(?P<identifier>injection_rate)(?P<assign_op>[ =]+)(?P<value>[.0-9]+)(?P<semicolon>;)")
+avg_packet_latency = re.compile(r"(?P<identifier>Packet latency average)(?P<assign_op>[ =]+)(?P<value>[.0-9]+)")
 rates = [round(rate * 0.01, 3) for rate in range(100)]
 
 
@@ -17,10 +18,17 @@ def invoke():
     :return:
     """
     with Popen(['booksim', config_filename], stdout=PIPE) as simulation:
+        overall_stats = False
         with open('simulation', 'wb') as file:
             for line in simulation.stdout:
-                file.write(line)
+                if "Overall Traffic" in str(line):
+                    overall_stats = True
+                if overall_stats:
+                    file.write(line)
 
+def get_values():
+    with open('simulation', 'r') as file:
+        avg_packet_latency.findall()
 
 def update_config(new_rate):
     with fileinput.input(config_filename, inplace=True) as config:
