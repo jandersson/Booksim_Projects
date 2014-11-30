f193550d35182496ec5003a4e38c6b31659685b4
Jonas Andersson <jonas@andersson.io>
2014-11-30 11:32:01 -0600

    various changes

--- invoke_sim.py
+++ invoke_sim.py
@@ -2,6 +2,7 @@ __author__ = 'Jonas Andersson'
 
 from subprocess import PIPE, Popen
 import re
+import json
 import fileinput
 import matplotlib.pyplot as plt
 
@@ -9,17 +10,13 @@ import matplotlib.pyplot as plt
 config_filename = "sim_config"
 inject_rate = re.compile(r"(?P<identifier>injection_rate)(?P<assign_op>[ =]+)(?P<value>[.0-9]+)(?P<semicolon>;)")
 avg_packet_latency = re.compile(r"(?P<identifier>Packet latency average)(?P<assign_op>[ =]+)(?P<value>[.0-9]+)")
-algorithm_re = re.compile(r"(?P<identifier>routing_function)(?P<assign_op>[ =]+)(?P<algorithm>[a-z]+)(?P<semicolon>;)")
+algorithm_re = re.compile(r"(?P<identifier>routing_function)(?P<assign_op>[ =]+)(?P<algorithm>[a-z_]+)(?P<semicolon>;)")
 rates = [round(rate * 0.01, 3) for rate in range(0, 100, 5)]
 algorithms = ['dor', 'romm', 'min_adapt', 'valiant']
 
 
 #Helper Functions
 def invoke():
-    """
-    This function invokes the simulator and dumps the output to a file
-    :return:
-    """
     with Popen(['booksim', config_filename], stdout=PIPE) as simulation:
         overall_stats = False
         with open('simulation', 'wb') as file:
@@ -40,14 +37,18 @@ def get_value():
         return latency
 
 
-def update_config(new_rate, algorithm=None):
-    if algorithm:
+def update_config(new_rate, alg=None):
+    if alg:
+        print('Simulating ' + alg + ' algorithm')
         with fileinput.input(config_filename, inplace=True) as config:
             for line in config:
-                print(algorithm_re.sub('routing_function = ' + str(algorithm) + ';', line), end='')
+                print(algorithm_re.sub('routing_function = ' + alg + ';', line), end='')
+        return
+    print('Injection rate set to ' + new_rate)
     with fileinput.input(config_filename, inplace=True) as config:
         for line in config:
             print(inject_rate.sub('injection_rate = ' + str(new_rate) + ';', line), end='')
+    return
 
 
 def run_simulation():
@@ -60,27 +61,28 @@ def run_simulation():
     return data
 
 
-def plot_data(data):
+def plot_data(results):
     xlabel = 'Offered Traffic'
     ylabel = 'Avg delay (cycles)'
-    all_x = []
-    all_y = []
-    for data_set in data:
+    for data_set in results:
         x_vals = [x[0] for x in data_set]
         y_vals = [y[1] for y in data_set]
-        all_x.append(x_vals)
-        all_y.append(y_vals)
-    plt.plot(all_x[0], all_y[0], all_x[1], all_y[1], all_x[2], all_y[2], all_x[3], all_y[3])
+        plt.plot(x_vals, y_vals)
     plt.ylabel(ylabel)
     plt.xlabel(xlabel)
     plt.show()
 
 
+def data_file(alg, dat):
+    with open(alg, 'w') as file:
+        file.write(json.dumps(dat))
+
 #Main Loop
 if __name__ == '__main__':
     all_data = []
     for algorithm in algorithms:
-        update_config('0.00', algorithm=algorithm)
+        update_config('0.00', alg=algorithm)
         data = run_simulation()
         all_data.append(data)
+        data_file(algorithm, data)
     plot_data(all_data)
