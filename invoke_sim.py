fac53747bbb8249e619a6d29441cb44a4f730ff7
Jonas Andersson <jonas123@gmail.com>
2014-12-08 22:48:43 -0600

    Project 4 completion

--- invoke_sim.py
+++ invoke_sim.py
@@ -6,14 +6,18 @@ import json
 import fileinput
 import matplotlib.pyplot as plt
 import csv
+from itertools import cycle
 
 #Global State
+data_filename = ""
 config_filename = "sim_config"
 inject_rate = re.compile(r"(?P<identifier>injection_rate)(?P<assign_op>[ =]+)(?P<value>[.0-9]+)(?P<semicolon>;)")
 avg_packet_latency = re.compile(r"(?P<identifier>Packet latency average)(?P<assign_op>[ =]+)(?P<value>[.0-9]+)")
 algorithm_re = re.compile(r"(?P<identifier>routing_function)(?P<assign_op>[ =]+)(?P<algorithm>[a-z_]+)(?P<semicolon>;)")
 num_vcs_re = re.compile(r"(?P<identifier>num_vcs)(?P<assign_op>[ =]+)(?P<value>[0-9]+)(?P<semicolon>;)")
-allocator_re = re.compile(r"(?P<identifier>)")
+allocator_re = re.compile(r"(?P<identifier>vc_allocator)(?P<assign_op>[ =]+)(?P<method>[a-z_]+)(?P<semicolon>;)")
+sw_allocator_re = re.compile(r"(?P<identifier>sw_allocator)(?P<assign_op>[ =]+)(?P<method>[a-z_]+)(?P<semicolon>;)")
+traffic_re = re.compile(r"(?P<identifier>traffic)(?P<assign_op>[ =]+)(?P<pattern>[a-z_]+)(?P<semicolon>;)")
 
 rates = [round(rate * 0.001, 3) for rate in range(0, 500, 2)]
 algorithms = ['dor', 'romm', 'min_adapt', 'valiant']
@@ -41,18 +45,27 @@ def get_value():
         return latency
 
 
-def update_config(new_rate, alg=None, vc_num=None, alloc=None):
+def update_config(new_rate, alg=None, vc_num=None, alloc=None, traffic=None):
+    if traffic:
+        print('Traffic pattern set to ' + traffic)
+        with fileinput.input(config_filename, inplace=True) as config:
+            for line in config:
+                print(traffic_re.sub('traffic = ' + traffic + ';', line), end='')
     if alloc:
-        print('Allocation set to ' + alloc)
+        #There has to be a better way to do this
+        #If the two sub calls are in the same loop, it fills the config file with garbage
+        print('Switch and VC allocation set to ' + alloc)
         with fileinput.input(config_filename, inplace=True) as config:
             for line in config:
                 print(allocator_re.sub('vc_allocator = ' + alloc + ';', line), end='')
-    if vc_num:
-        print('Setting number of virtual channels to: ' + vc_num)
         with fileinput.input(config_filename, inplace=True) as config:
             for line in config:
-                print(num_vcs_re.sub('num_vcs = ' + vc_num + ';', line), end='')
-
+                print(sw_allocator_re.sub('sw_allocator = ' + alloc + ';', line), end='')
+    if vc_num:
+        print('Setting number of virtual channels to: ' + str(vc_num))
+        with fileinput.input(config_filename, inplace=True) as config:
+            for line in config:
+                print(num_vcs_re.sub('num_vcs = ' + str(vc_num) + ';', line), end='')
     if alg:
         print('Simulating ' + alg + ' algorithm')
         with fileinput.input(config_filename, inplace=True) as config:
@@ -76,17 +89,19 @@ def run_simulation():
     return data
 
 
-def plot_data(results):
+def plot_data(results, legend):
     xlabel = 'Offered Traffic'
     ylabel = 'Avg delay (cycles)'
     title = 'Average Latency vs. Offered Load'
+    lines = ["-","--","-.",":"]
+    linecycler = cycle(lines)
     for data_set in results:
         x_vals = [x[0] for x in data_set]
         y_vals = [y[1] for y in data_set]
-        plt.plot(x_vals, y_vals)
+        plt.plot(x_vals, y_vals, next(linecycler))
     plt.figure(1)
     plt.title(title)
-    plt.legend(algorithms)
+    plt.legend(legend, loc='upper left')
     plt.ylabel(ylabel)
     plt.xlabel(xlabel)
     plt.show()
@@ -96,22 +111,22 @@ def make_table(contents):
     pass
 
 
-def save_data(alg, dat):
-    with open(alg, 'w') as file:
+def save_data(alg, num_vcs, traffic, allocator, dat):
+    data_filename = alg + '-' + str(num_vcs) + '-'  + traffic + '-'  + allocator
+    with open(data_filename, 'w') as file:
         file.write(json.dumps(dat))
-    with open(alg + '.csv', 'w', newline='') as file:
+    with open(data_filename + '.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(dat)
 
 
-def load_data():
+def load_data(alg, num_vcs, traffic, allocator):
+    data_filename = alg + '-' + str(num_vcs) + '-'  + traffic + '-'  + allocator
     all_data = []
-    for alg in algorithms:
-        with open(alg, 'r') as file:
-            text = file.read()
-            data = json.loads(text)
-            all_data.append(data)
-    return all_data
+    with open(data_filename, 'r') as file:
+        text = file.read()
+        data = json.loads(text)
+    return data
 
 
 #Main Loop
--- project4.py
+++ project4.py
@@ -16,8 +16,35 @@
 
 __author__ = 'Jonas Andersson'
 
-#Global State
+import invoke_sim
 
+#Global State
+algorithm = "dim_order"
 num_vcs = [8, 16]
 traffic = ['uniform', 'neighbor']
 allocators = ['wavefront', 'separable_input_first']
+
+#Main Loop
+if __name__ == "__main__":
+    simulate = False
+    load_previous = True
+    all_data = []
+    if simulate:
+        for vcs in num_vcs:
+            for pattern in traffic:
+                for method in allocators:
+                    invoke_sim.update_config('0.00', alloc=method, vc_num=vcs, traffic=pattern)
+                    data = invoke_sim.run_simulation()
+                    all_data.append(data)
+                    invoke_sim.save_data(algorithm, vcs, pattern, method, data)
+        invoke_sim.plot_data(all_data)
+    elif load_previous:
+        legend = []
+        print("Loading previous simulation")
+        for vcs in num_vcs:
+            for pattern in traffic:
+                for method in allocators:
+                    data = invoke_sim.load_data(alg=algorithm, num_vcs=vcs, traffic=pattern, allocator=method)
+                    all_data.append(data)
+                    legend.append(algorithm + '-' + str(vcs) + '-'  + pattern + '-'  + method)
+        invoke_sim.plot_data(all_data, legend)
